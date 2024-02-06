a95403f731919865cfa0c89356927ff3f17af4d1
Ian Miller <imiller142@gmail.com>
2024-02-06 13:27:27 -0600

    database init

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+﻿DROP DATABASE IF EXISTS just_tech_news_db;
+
+CREATE DATABASE just_tech_news_db;
---
+++ models/User.js
@@ -0,0 +1,42 @@
+﻿const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create User model
+class User extends Model {}
+
+//define table columns and configuration
+User.init(
+    {
+    //define an id column
+    id: {
+        //use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        //this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        //instruct that this is the primary key
+        primaryKey: true,
+        //turn on auto increment
+        autoIncrement: true
+    },
+    // define a username column
+    username: {
+
+    }
+
+    },
+
+    {
+        //table configuration options go here
+
+        //pass in our imported sequelize connection
+        sequelize,
+        // dont automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        //dont pluralize name of database table
+        freezeTableName: true,
+        //use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+        underscored: true,
+        //make it so our model name stays lowercase in the database
+        modelName: 'user'
+    }
+);
