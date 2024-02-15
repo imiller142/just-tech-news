685d9559ea55d0f68cf3ff5219c0367a0e3c3621
Ian Miller <imiller142@gmail.com>
2024-02-15 13:34:14 -0600

    finalize and refactor

--- models/User.js
+++ models/User.js
@@ -1,86 +1,63 @@
 ﻿const { Model, DataTypes } = require('sequelize');
-
 const bcrypt = require('bcrypt');
-
 const sequelize = require('../config/connection');
 
-//create User model
+// create our User model
 class User extends Model {
-    //set up method to check password vs hashed password.
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
-//define table columns and configuration
+// create fields/columns for User model
 User.init(
-    {
-    //define an id column
+  {
     id: {
-        //use the special Sequelize DataTypes object provide what type of data it is
-        type: DataTypes.INTEGER,
-        //this is the equivalent of SQL's `NOT NULL` option
-        allowNull: false,
-        //instruct that this is the primary key
-        primaryKey: true,
-        //turn on auto increment
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
-    // define a username column
     username: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    //defin an email collumn
     email: {
-
-        type: DataTypes.STRING,
-        allowNull: false,
-        //there cannot be any duplicate email values in this table
-        unique: true,
-        // if allownull is set to false, we can run our data through validators before creating the table data
-        validate: {
-            isEmail: true
-        }
-    },
-    //define password column
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            //this means the password must be at  least four characters long
-            len: [4]
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
       }
     },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+    }
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
 
-    {
-        //table configuration options go here
-            hooks: {
-                //set up before create lifecycle hook functionality
-                async beforeCreate(newUserData) {
-                    newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                    return newUserData;
-                },
-                // set up beforeUpdate lifecycle "hook" functionality
-                async beforeUpdate(updatedUserData) {
-                    updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                    return updatedUserData;
-                }
-            },
-        //pass in our imported sequelize connection
-        sequelize,
-        // dont automatically create createdAt/updatedAt timestamp fields
-        timestamps: false,
-        //dont pluralize name of database table
-        freezeTableName: true,
-        //use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-        underscored: true,
-        //make it so our model name stays lowercase in the database
-        modelName: 'user'
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
     },
-
-
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
 );
 
 module.exports = User;
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -4,61 +4,62 @@ const { Post, User, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
-    console.log('======================');
-    Post.findAll({
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-      order: [['created_at', 'DESC']],
-      include: [
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
+  console.log('======================');
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    order: [['created_at', 'DESC']],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
     })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      })
-
-  });
-
-
-//get one
-  router.get('/:id', (req, res) => {
-    Post.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
-      include: [
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(dbPostData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
-//create
 router.post('/', (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
@@ -73,41 +74,15 @@ router.post('/', (req, res) => {
     });
 });
 
-// /api/posts/upvote
-
 router.put('/upvote', (req, res) => {
-  // create the vote
-  Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id
-  }).then(() => {
-    // then find the post we just voted on
-    return Post.findOne({
-      where: {
-        id: req.body.post_id
-      },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
-        [
-          sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-          'vote_count'
-        ]
-      ]
-    })
-    .then(dbPostData => res.json(dbPostData))
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
     .catch(err => {
       console.log(err);
       res.status(400).json(err);
     });
-  });
 });
-
-
-//update post
 
 router.put('/:id', (req, res) => {
   Post.update(
@@ -133,7 +108,6 @@ router.put('/:id', (req, res) => {
     });
 });
 
-//delete post
 router.delete('/:id', (req, res) => {
   Post.destroy({
     where: {
@@ -153,4 +127,4 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-  module.exports = router;
+module.exports = router;
